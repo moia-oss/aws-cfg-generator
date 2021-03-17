8fba88be85af2f092e8dc5b141c23bc546da8fd7
Nathan Beneke <14852919+nathan-beneke@users.noreply.github.com>
2021-03-17 12:20:25 -0500

    Update pkg/util/ini.go
    Co-authored-by: Nihal Gonsalves <nihal@nihalgonsalves.com>

--- pkg/util/ini.go
+++ pkg/util/ini.go
@@ -13,13 +13,18 @@ package util
    limitations under the License.
 */
 
-import "gopkg.in/ini.v1"
+import (
+	"github.com/rs/zerolog/log"
+	"gopkg.in/ini.v1"
+)
 
 func GetKeySetter(section *ini.Section) func(key, value string) {
 	return func(key, value string) {
 		_, err := section.NewKey(key, value)
 		if err != nil {
-			panic(err)
+			log.Panic().Err(err).
+				Str("section", section.Name()).Str("key", key).Str("val", value).
+				Msg("could not set")
 		}
 	}
 }
