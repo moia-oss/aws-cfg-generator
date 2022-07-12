13ceb49890b6c0a4c93b1d0fa9a0ba51a92cf2b0
Max Opperman <55201171+oppermax@users.noreply.github.com>
2022-07-12 17:48:44 +0200

    add function to order a slice of profiles

---
+++ pkg/util/order.go
@@ -0,0 +1,43 @@
+package util
+
+/*
+  Copyright 2021 MOIA GmbH
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import (
+	"golang.org/x/exp/slices"
+	"strings"
+)
+
+func profileLess(x, y Profile) bool {
+	return x.ProfileName < y.ProfileName
+}
+
+func OrderProfiles(unorderedProfiles []Profile) []Profile {
+	var singleProfiles, stagedProfiles []Profile
+
+	stages := []string{"poc", "stg", "dev", "int", "prd"}
+
+	for _, profile := range unorderedProfiles {
+		for _, stage := range stages {
+			if strings.HasSuffix(profile.ProfileName, stage) {
+				stagedProfiles = append(stagedProfiles, profile)
+			} else {
+				singleProfiles = append(singleProfiles, profile)
+			}
+		}
+	}
+	slices.SortFunc(singleProfiles, profileLess)
+	slices.SortFunc(stagedProfiles, profileLess)
+
+	return append(singleProfiles, stagedProfiles...)
+}
